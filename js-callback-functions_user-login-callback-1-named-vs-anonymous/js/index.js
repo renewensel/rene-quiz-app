526daac938ebfa92ad9d638f4a5e77a45cc3d0ed
renewensel <hey@renewensel.com>
2023-10-11 15:10:39 +0200

    js-callback-functions_user-login solution

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -2,7 +2,19 @@ console.clear();
 
 function handleUserLogin(onSuccess) {
   // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  // console.log("Hello from handleUserLogin");
   onSuccess();
 }
 
 // Call handleUserLogin below!
+function showWelcomeMessage() {
+  console.log("Welcome!");
+}
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {
+  {
+    console.log("Welcome! From anonymous function.");
+  }
+});
