cbf9907ff13973cb3fb191b794a22df404b1f540
renewensel <hey@renewensel.com>
2023-10-12 17:11:53 +0200

    bookmark toggle works

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 console.clear();
 console.log("Hello from index.js");
 
+/*******        Buttons for showing / hiding answers         ********/
+
+// query buttons and answer cards
 const answerButtons = document.querySelectorAll('[data-js="answer-button"]');
 const answerShowHide = document.querySelectorAll('[data-js="card__answer"]');
 
@@ -12,5 +15,15 @@ answerButtons.forEach((button, index) => {
         } else {
             button.textContent = "Hide Answer";
         }
+    });
+});
+
+/*******        Bookmark select / deselect         ********/
+const bookmarks = document.querySelectorAll('[data-js="bookmark');
+console.log(bookmarks);
+
+bookmarks.forEach((bookmark, index) => {
+    bookmark.addEventListener("click", () => {
+        bookmarks[index].classList.toggle("bookmark_active");
     });
 });
